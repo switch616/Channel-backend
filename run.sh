8f46b63b46f5caea487d10ad70b0721c1f565571
switch616 <3206848502@qq.com>
2026-01-08 00:29:19 +0800

    auto commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate py310_web
 port=8088; pid=$(lsof -t -i:$port); [ -n "$pid" ] && echo "kill port $port pid $pid" && kill -9 $pid || echo "port $port 未被占用"
-export APP_ENV=dev
+export APP_ENV=test
 python3 main.py
